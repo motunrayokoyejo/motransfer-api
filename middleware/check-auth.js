c415b373f7b6c172e3cd40228afe450bc860c073
Motunrayo Koyejo <koyejomotunrayo@gmail.com>
2021-05-21 15:27:12 +0100

    fixed bugs

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -4,15 +4,12 @@ module.exports = (req, res, next) => {
        try{
         const token = req.headers.authorization.split(" ")[1];
         // console.log(token);
-        const decoded = jwt.verify(token, process.env.JWT_key);
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.userData = decoded;
         next();
      
        } catch(error){
 
-        //    return res.status(401).json({
-        //        message: "Auth failed"
-        //    });
         console.log(`Authentication error `, error.message);
            return res.status(401).json({
                message: "Auth failed"
